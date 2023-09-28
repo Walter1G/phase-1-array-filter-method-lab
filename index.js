91e7eda1be8bd9defd27c89da6a054f26d98a93a
Walter N <2760walter@gmail.com>
2023-09-28 20:51:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Code your solution here
+
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+// function findMatching(list, arg){
+
+//     let newList =[];
+//         for(let i=0; i<list.length;i++){
+            
+//             if ( list[i].toUpperCase() === arg.toUpperCase()){
+//                 newList.push(list[i]);               
+//             }           
+//         }
+//         return newList;
+// }
+
+function findMatching(drivers, str) {
+    
+    const lowercaseQstr = str.toLowerCase();
+  
+    
+    const matchingDrivers = drivers.filter(driver => {
+      const lowercaseDriverName = driver.toLowerCase();
+      return lowercaseDriverName === lowercaseQstr;
+    });
+  
+    return matchingDrivers;
+  }
+
+
+
+function fuzzyMatch(drivers, str) {
+    
+    const lowercasestr = str.toLowerCase();
+    
+    
+    const matchingDrivers = drivers.filter(driver => {
+      const lowercaseDriverName = driver.toLowerCase();
+      return lowercaseDriverName.startsWith(lowercasestr);
+    });
+    
+    return matchingDrivers;
+  }
+
+  function matchName(drivers, name) {
+
+    const matchingDrivers = drivers.filter(driver => {
+      return driver.name === name;
+    });
+    
+    return matchingDrivers;
+  }
+  
+
